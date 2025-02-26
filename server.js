e52a2c46dd328c8ecdb22881b4dc8322f5065509
Артур Шакуров <arthur@Arturs-MacBook-Air.local>
2025-02-26 11:03:36 +0500

    adding heroku

--- server.js
+++ server.js
@@ -1,4 +1,16 @@
-const mongoose = require('mongoose');
-const dotenv = require('dotenv');
-const app = require('./app');
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const app = require("./app");
 
+dotenv.config({ path: "./config.env" });
+
+const DB = process.env.DATABASE;
+mongoose.connect(DB).then(() => {
+  console.log("DB connection successful!");
+});
+
+const port = process.env.PORT;
+
+app.listen(port, () => {
+  console.log(`App running on port ${port}`);
+});
